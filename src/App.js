fafdd80b31fe7f3b4c6de35422cb4ecb5574576d
sumitchoudharySEM <sumitsem2004@gmail.com>
2023-03-05 19:57:09 +0530

    basic location and finding all address functions build

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+  const [walletAddress, setWalletAddress] = useState("");
+
+  async function requestAccount() {
+    console.log("hii");
+    if(window.ethereum) {
+      console.log("metamask detected");
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        console.log(accounts);
+        setWalletAddress(accounts[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    }else {
+      console.log("metamask not detected");
+    }
+    // window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={requestAccount}>Connect Metamask Wallet</button>
+        <h4>Wallet Address: {walletAddress}</h4>
       </header>
     </div>
   );
