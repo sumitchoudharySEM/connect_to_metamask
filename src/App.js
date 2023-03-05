69d7e98fe0fe870addd47a0445ba01b7dcf660f1
sumitchoudharySEM <sumitsem2004@gmail.com>
2023-03-05 20:22:26 +0530

    connecting wallet functanolity done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
-import "./App.css";
-import { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import WalletCard from './WalletCard';
 
 function App() {
 
-  const [walletAddress, setWalletAddress] = useState("");
-
-  async function requestAccount() {
-    console.log("hii");
-    if(window.ethereum) {
-      console.log("metamask detected");
-      try {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        console.log(accounts);
-        setWalletAddress(accounts[0]);
-      } catch (error) {
-        console.log(error);
-      }
-    }else {
-      console.log("metamask not detected");
-    }
-    // window.ethereum.request({ method: 'eth_requestAccounts' });
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={requestAccount}>Connect Metamask Wallet</button>
-        <h4>Wallet Address: {walletAddress}</h4>
-      </header>
+    <WalletCard/>
+
     </div>
   );
 }
